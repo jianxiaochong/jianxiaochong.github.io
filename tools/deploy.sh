16f0358ff04b843e1e34df9e8c59e0d39694c6a5
jianxiaochong <814105368@qq.com>
2021-04-02 16:54:14 +0800

    first commit

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Deploy the content of _site to 'origin/<pages_branch>'
-#
-# v2.5
-# https://github.com/cotes2020/jekyll-theme-chirpy
-# © 2020 Cotes Chung
-# Published under MIT License
 
 set -eu
 
@@ -15,6 +10,17 @@ _no_branch=false
 _backup_dir="$(mktemp -d)"
 
 init() {
+  if [[ -z ${GITHUB_ACTION+x} ]]; then
+    echo "ERROR: This script is not allowed to run outside of GitHub Action."
+    exit -1
+  fi
+
+  # Gemfile could be changed by `bundle install` in actions workflow
+  if [[ -n $(git ls-files | grep Gemfile.lock) && -n \
+  $(git status Gemfile.lock --porcelain) ]]; then
+    git checkout -- Gemfile.lock
+  fi
+
   if [[ -z $(git branch -av | grep "$PAGES_BRANCH") ]]; then
     _no_branch=true
     git checkout -b "$PAGES_BRANCH"
@@ -42,7 +48,7 @@ flush() {
   mv "$_backup_dir"/* .
 }
 
-deoply() {
+deploy() {
   git config --global user.name "GitHub Actions"
   git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
 
@@ -61,7 +67,7 @@ main() {
   init
   backup
   flush
-  deoply
+  deploy
 }
 
 main
